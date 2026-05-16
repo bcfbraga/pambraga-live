0570fb23b4eb0acda4132746ca3b35a22d88cf88
bcfbraga <bcfbraga@gmail.com>
2026-05-16 13:01:07 -0300

    Remove intro section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-import { IntroBlock } from "@/components/IntroBlock";
 import { ProductGrid } from "@/components/ProductGrid";
 import { ShareBlock } from "@/components/ShareBlock";
 import { liveInfo, products } from "@/data/live";
@@ -8,8 +7,7 @@ import { liveInfo, products } from "@/data/live";
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden">
-      <Hero liveInfo={liveInfo} productCount={products.length} />
-      <IntroBlock influencerName={liveInfo.influencerName} />
+      <Hero liveInfo={liveInfo} />
       <ProductGrid products={products} influencerName={liveInfo.influencerName} />
       <ShareBlock />
       <Footer liveInfo={liveInfo} />
