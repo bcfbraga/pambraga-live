2e6168b1dc19b2b0f6ad083e244e0becfd61ad68
bcfbraga <bcfbraga@gmail.com>
2026-05-17 09:42:16 -0300

    Pass product count to hero

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { liveInfo, products } from "@/data/live";
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden">
-      <Hero liveInfo={liveInfo} />
+      <Hero liveInfo={liveInfo} productCount={products.length} />
       <ProductGrid products={products} influencerName={liveInfo.influencerName} />
       <ShareBlock />
       <Footer liveInfo={liveInfo} />
