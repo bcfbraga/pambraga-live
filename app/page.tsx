0b1c770c83c023ad5614e4f8c4b1547d58818aa3
bcfbraga <bcfbraga@gmail.com>
2026-05-26 16:45:21 -0300

    Show top live bar on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "@/components/Footer";
+import { LiveBar } from "@/components/LiveBar";
 import { ProductGrid } from "@/components/ProductGrid";
 import { ShareBlock } from "@/components/ShareBlock";
 import { liveInfo, products } from "@/data/live";
@@ -6,6 +7,7 @@ import { liveInfo, products } from "@/data/live";
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden">
+      <LiveBar liveInfo={liveInfo} />
       <ProductGrid products={products} influencerName={liveInfo.influencerName} />
       <ShareBlock />
       <Footer liveInfo={liveInfo} />
